407c43c4355c2f464f261ad229078f01da191b9d
torizukayuki <yuki_torizuka@keio.jp>
2019-03-15 10:56:11 +0900

    1-1の解答を追加

---
+++ p2_ニューラルネットワーク実装/my_answer/ex1-1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Perceptron:
+    def __init__(self, w1, w2, theta):
+        self.w1 = w1
+        self.w2 = w2
+        self.theta = theta
+
+    def forward(self, x1, x2):
+        if (self.w1 * x1 + self.w2 * x2) >= self.theta:
+            return 1
+        else:
+            return 0
+
+
